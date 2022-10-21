3731ccdf4fd3970afc1d0957c53ba513acb9fec8
Marina Victoria Blanco <marivic_05@hotmail.com>
2022-10-21 20:05:35 -0300

    Sección about ok

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import styles from "../styles/section.module.css";
-import { faCircleDot} from "@fortawesome/free-regular-svg-icons";
+import { faCircleDot } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import nosotros from "../assets/img/aboutUs.jpg";
 
 const About = () => {
-
- 
   return (
     <section className={styles.section}>
       <Container>
@@ -18,32 +17,38 @@ const About = () => {
         <article className={styles.pSection}>
           <p>
             Somos una empresa familiar que,
-            <strong>desde 1984, nos especializamos</strong> en la reparación de
+            <strong> desde 1984, nos especializamos</strong> en la reparación de
             electrodomésticos de línea blanca, para clientes residenciales y
             empresas.
           </p>
-          <p >
-            Los pilares de nuestro trabajo son:
-            <ul className={styles.pilares}>
-              <li><FontAwesomeIcon icon={faCircleDot} color="#D2001A" size="small"/> Cumplimiento y Puntualidad;</li>
-              <li><FontAwesomeIcon icon={faCircleDot} color="#D2001A"/> Calidad en atención;</li>
-              <li><FontAwesomeIcon icon={faCircleDot} color="#D2001A"/> Responsabilidad y Confiabilidad.</li>
-            </ul>
-          </p>
+          
+            <div className={styles.pilaresImg}>
+              <ul className={styles.pilares}>
+                <span>Los pilares de nuestro trabajo son:</span>
+                <li>
+                  <FontAwesomeIcon
+                    icon={faCircleDot}
+                    color="#D2001A"
+                    size="small"
+                  />  Cumplimiento y Puntualidad;
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCircleDot} color="#D2001A" />  Calidad
+                  en atención;
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCircleDot} color="#D2001A" />  Responsabilidad y Confiabilidad.
+                </li>
+              </ul>
+              <picture>
+                <img
+                  src={nosotros}
+                  alt="Empresa familiar"
+                  className={styles.imgAbout}
+                />
+              </picture>
+            </div>
         </article>
-
-        <picture>
-          <source
-            srcset="land-small-car-image.jpg 200w,
-                land-medium-car-image.jpg 600w,
-                land-large-car-image.jpg 1000w"
-            sizes="(min-width: 700px) 500px,
-               (min-width: 600px) 400px,
-               100vw"
-          />
-
-          <img src="land-medium-car-image.jpg" alt="Car" />
-        </picture>
       </Container>
     </section>
   );
